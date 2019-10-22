ca1636915e7c591a894202c3985f60b38b6ea267
Florian Thiévent <florianjeremias@gmail.com>
2019-10-22 12:14:37 +0200

    added 06_Composite

--- 03_Observer2/src/main/java/patterns/observer/memory/MulticastPanel.java
+++ 03_Observer2/src/main/java/patterns/observer/memory/MulticastPanel.java
@@ -1,48 +1,61 @@
 package patterns.observer.memory;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 // A panel with buttons to create and close sample frames.
 @SuppressWarnings("serial")
 public class MulticastPanel extends JPanel implements ActionListener {
-	private int counter = 0;
-	private JButton closeAllButton;
+    private int counter = 0;
+    private JButton closeAllButton;
 
-	public MulticastPanel() {
-		JButton newButton = new JButton("New");
-		this.add(newButton);
-		newButton.addActionListener(this);
+    public MulticastPanel() {
+        JButton newButton = new JButton("New");
+        this.add(newButton);
+        newButton.addActionListener(this);
 
-		closeAllButton = new JButton("Close all");
-		this.add(closeAllButton);
-	}
+        closeAllButton = new JButton("Close all");
+        this.add(closeAllButton);
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent evt) {
-		SimpleFrame f = new SimpleFrame();
-		counter++;
-		f.setTitle("Window " + counter);
-		f.setSize(250, 150);
-		f.setVisible(true);
-		closeAllButton.addActionListener(f);
-	}
+    @Override
+    public void actionPerformed(ActionEvent evt) {
 
-	private class SimpleFrame extends JFrame implements ActionListener {
-		byte[][] buf = new byte[1024][];
-		{
-			for (int i = 0; i < 1024; i++) {
-				buf[i] = new byte[1024 * 256];
-			}
-		}
+        // 3. Check current heap size if possible for new window
+        System.out.printf("current mem %d \n", Runtime.getRuntime().totalMemory());
+        System.out.printf("max mem %d\n", Runtime.getRuntime().maxMemory());
 
-		@Override
-		public void actionPerformed(ActionEvent evt) {
-			this.dispose();
-		}
-	}
+        SimpleFrame f = new SimpleFrame();
+        counter++;
+        System.out.println(counter);
+        f.setTitle("Window " + counter);
+        f.setSize(250, 150);
+        f.setVisible(true);
+        closeAllButton.addActionListener(f);
+    }
+
+    private class SimpleFrame extends JFrame implements ActionListener {
+        byte[][] buf = new byte[1024][];
+
+        {
+            for (int i = 0; i < 1024; i++) {
+                buf[i] = new byte[1024 * 256];
+            }
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+
+            // 1.
+            closeAllButton.removeActionListener(this);
+
+            // 2.
+            System.runFinalization();
+            System.gc();
+
+
+            this.dispose();
+        }
+    }
 }
